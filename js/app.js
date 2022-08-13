8916bca5726598535d93676b7a57925cf6cf4516
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-13 16:27:00 -0700

    starting from scratch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict'
+// Create a "close" button and append it to each list item
+
+// Click on a close button to hide the current list item
+
+
+// Add a "checked" symbol when clicking on a list item
+
+
+// Create a new list item when clicking on the "Add" button
+
+
+// Create a new favorite item when clicking on the "Add to Favorites" button
