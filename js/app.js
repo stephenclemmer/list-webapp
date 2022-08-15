44ea868bce17bb4a03079c9d4c62268d1278adbf
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-08-14 17:20:17 -0700

    created checkboxes that, when actuated, allow text to be struckthrough.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 // Event Listener for "Add to List"
-const myForm = document.getElementById("myForm");
-myForm.addEventListener("submit", newListItem);
+const myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', newListItem);
 
 const listArray = [];
 
@@ -9,12 +9,29 @@ const listArray = [];
 function newListItem(event){
   event.preventDefault();
 
-  let newItem = document.getElementById("newInput").value;
+  let newItem = document.getElementById('newInput').value;
   listArray.push(newItem);
 
-  let unorderedList = document.getElementById("list");
-  let listItem = document.createElement("li");
+  let unorderedList = document.getElementById('list');
+  let listItem = document.createElement('li');
 
-  listItem.appendChild(document.createTextNode(listArray[listArray.length - 1]));
+// create checkboxes next to each item
+
+  let checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = 'li';
+  checkbox.name = 'checkbox';
+  checkbox.value = 1;
+  checkbox.classList.add('li');
+
+// Create label to display text and nest inside li
+  let label = document.createElement('label');
+  label.htmlFor = 'li';
+  label.classList.add('strikethrough');
+  label.appendChild(document.createTextNode(listArray[listArray.length - 1]));
+
+// Append checkbox and label to li and then to ul
+  listItem.appendChild(checkbox);
+  listItem.appendChild(label);
   unorderedList.appendChild(listItem);
 }
